40bae2621e62f7298c50d3885d5c68aa1ebfc7cb
Askar Mamytov <askar.mamytov201@gmail.com>
2024-07-13 12:41:55 -0400

    minor touches and comments

--- analize_log.sh
+++ analize_log.sh
@@ -2,20 +2,16 @@
 
 shopt -s lastpipe
 
+#If file 'server.log' exists, remove it
 if [ -f ./server.log ]; then
     rm server.log
     echo "server.log file found. Removing it"
 fi
 
+#Download and save in new 'server.log'
 wget https://raw.githubusercontent.com/akjolkg/tntk/4f47929e79726836a170d25f13e0279136cb29f9/tntk.log -O server.log
 
-# if [ $? -eq 0 ]; then
-#     cat ./server.log
-# fi
-
-#URLS
 #User Activity Pattern
-
 count_pending_messages='count_pending_messages'
 get_messages='get_messages'
 get_friends_messages='get_friends_progress'
@@ -23,9 +19,7 @@ get_friends_score='get_friends_score'
 fb_request_messages='fb_request_messages'
 files="files"
 
-arr=($count_pending_messages $get_messages $get_friends_messages $get_friends_score $fb_request_messages $files)
-
-echo ${arr[@]}
+arr=("$count_pending_messages" "$get_messages" "$get_friends_messages" "$get_friends_score" "$fb_request_messages" "$files")
 
 # Functions
 
@@ -34,6 +28,7 @@ count_lines() {
     cat server.log | grep "$1" | wc -l
 }
 
+# Function to count service and connect times
 count_request_times() {
     grep "$1" server.log | while read -r line; do
         field_num=$2  # Set this dynamically as needed
@@ -45,18 +40,18 @@ count_request_times() {
 
 
 
-final_function() {
-
+consolidate() {
     local sum=0
-    #count service
 
+    #count service and connect (updates local sum)
     count_request_times $1 9
     count_request_times $1 10
 
     #count words
     local lines=$(count_lines $1)
     echo "Total requests for $1: $lines"
-    # local avg=$(($sum/$lines))
+    
+    #count average including decimals
     local avg=$(echo "scale=2; $sum / $lines" | bc)
     echo "avg time for $1 is: $avg"
 }
@@ -64,10 +59,12 @@ final_function() {
 
 print_analitics() {
     for item in ${arr[@]}; do
-        final_function $item
         echo "======================"
+        consolidate $item
     done
 }
+
+echo "======================"
 
 print_analitics
 
